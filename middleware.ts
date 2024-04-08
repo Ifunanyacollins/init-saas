af7a559454e5b1783aa0c0b841186a0ac3e30592
Collins Ogbuzuru <collinsogbuzuru@gmail.com>
2024-04-08 16:23:22 +0100

    chore: update the comment

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import type { NextRequest } from 'next/server'
 const intlMiddleware = createMiddleware({
   // A list of all locales that are supported
   locales: ['en', 'de'],
-
+//don't show the locale in the url for example /en/about-us would be /about
   localePrefix: 'never',
   // Used when no locale matches
   defaultLocale: 'en'
